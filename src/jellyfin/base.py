ab7ef30a7a2f37bd335d6f0fdc31e3f9e3793540
Webysther Sperandio <webysther@gmail.com>
2025-09-04 01:29:52 +0200

    Refactor GeneratedModelWrapper and GeneratedListWrapper classes for improved clarity and functionality; remove unused dependencies and methods.

--- src/jellyfin/base.py
+++ src/jellyfin/base.py
@@ -1,78 +1,73 @@
-import jmespath, pandas
-
+from itertools import islice
 from logging import warning
 
-from typing import Any, Dict, List, Protocol
-from collections.abc import Iterable
+from typing import (
+    Any, 
+    Dict, 
+    List, 
+    Protocol, 
+    Callable, 
+    TypeVar, 
+    Generic
+)
+
+from collections.abc import Iterable, Sequence
 from pydantic import BaseModel
 from .generated import BaseItemDtoQueryResult
 
-class CollectionPagination(Protocol):
-    """ Protocol for paginated collections """
+JellyfinModel = TypeVar('JellyfinModel', bound=BaseModel)
+
+class Pagination(Protocol):
+    """ Protocol for paginated responses. """
     def next_page(self) -> BaseModel: ...
 
-class Model(BaseModel):
-    def to_str(self) -> str:
-        """ Signature method for generated models"""
-        return self._data.to_str()
-
-    def to_json(self) -> str:
-        """ Signature method for generated models"""
-        return self._data.to_json()
+class GeneratedModelWrapper(Generic[JellyfinModel]):
+    _model: JellyfinModel
     
-    def to_dict(self) -> Dict[str, Any]:
-        """ Signature method for generated models"""
-        return self._data.to_dict()
-    
-    def __str__(self) -> str:
-        """Returns the string representation of the model."""
-        return self.to_str()
-
-    def __getattr__(self, name: str) -> Any:
-        """Returns the value of the specified attribute from the item data.
-        
-        Args:
-            name (str): The name of the attribute to retrieve.
-            
-        Returns:
-            Any: The value of the specified attribute, or None if not found.
-        """
-        try:
-            return super().__getattr__(name)
-        except AttributeError:
-            pass
-        
-        try:
-            if self._data and hasattr(self._data, name):
-                return getattr(self._data, name)
-        except Exception:
-            return None
-
-        return None
+    def __init__(self, model: JellyfinModel):
+        self._model = model
 
     @property
-    def to_df(self) -> pandas.DataFrame:
-        """ Returns a DataFrame """
-        data = self.to_dict()
+    def model(self) -> BaseModel:
+        """ Returns the generated model """
+        return self._model
+    
+    @property
+    def items(self) -> List[BaseModel] | None:
+        """ Returns the list of items if present in the model, otherwise None """
+        return self._items
 
-        if isinstance(data, list):
-            return pandas.DataFrame(data)
-        return pandas.DataFrame([data])
-
+    def __str__(self) -> str:
+        """Returns the string representation of the model."""
+        return str(self.model)
+    
+    def __repr__(self) -> str:
+        """Returns a detailed string representation of the Item object, with each attribute on a new line."""
+        attrs = []
+        if hasattr(self.model.__class__, "model_fields"):
+            keys = self.model.__class__.model_fields.keys()
+        else:
+            keys = self.model.__dict__.keys()
+        for key in keys:
+            value = getattr(self.model, key, None)
+            attrs.append(f"  {key}={value!r}")
+        attrs_str = ",\n".join(attrs)
+        return f"<{self.__class__.__name__}\n{attrs_str}\n>"
+    
     def summary(self, empty: bool = False, size: int = 80, limit: int = 100) -> None:
         """Prints the simple representation of the model.
         
         Args:
             empty (bool): If True, includes all values. If False, omits empty values (None, "", 0, [], {}).
         """
-        if hasattr(self._data.__class__, "model_fields"):
-            keys = self._data.__class__.model_fields.keys()
+        if hasattr(self.model.__class__, "model_fields"):
+            keys = self.model.__class__.model_fields.keys()
         else:
-            keys = self._data.__dict__.keys()
-        for key in keys:
-            if limit and limit <= 0:
+            keys = self.model.__dict__.keys()
+        for i, key in enumerate(keys):
+            if limit and i >= limit:
                 break
-            value = getattr(self._data, key, None)
+            value = getattr(self.model, key, None)
             if not empty:
                 if value is None or value == 0 or value == "":
                     continue
@@ -80,201 +75,60 @@ class Model(BaseModel):
                 if isinstance(value, (list, dict, set, tuple)) and not value:
                     continue
 
-            value_str = str(getattr(self._data, key))
+            value_str = str(getattr(self.model, key))
             if len(value_str) > size:
                 value_str = value_str[:size].rstrip() + "..."
 
-            limit -= 1
             print(f"{key}={value_str}")
 
-    def search(self, term: str) -> any:
-        """Extract value using a simplified XPath-like syntax.
-        
-        Args:
-            term (str): The XPath-like path to the desired attribute.
-
-        Returns:
-            any: The value at the specified path, or None if not found.
-        """
-        return jmespath.search(term, self.to_dict())
-
-class Single(Model):
-    _naming: str = "Single"
-    _data: BaseModel = None
+class GeneratedListWrapper(Sequence):
+    _model: GeneratedModelWrapper
+    _data: List[BaseModel]
     
-    def __init__(self, item: BaseModel):
-        """
-        Initializes the Item class.
-
-        Args:
-            item (BaseModel): The single item data.
-        """
-        self._data = item
+    def __init__(self, data: List[BaseModel] | GeneratedModelWrapper):
+        self._data = data
         
-    def __repr__(self) -> str:
-        """Returns a detailed string representation of the Item object, with each attribute on a new line."""
-        attrs = []
-        if hasattr(self._data.__class__, "model_fields"):
-            keys = self._data.__class__.model_fields.keys()
-        else:
-            keys = self._data.__dict__.keys()
-        for key in keys:
-            value = getattr(self._data, key, None)
-            attrs.append(f"  {key}={value!r}")
-        attrs_str = ",\n".join(attrs)
-        return f"<{self._naming}\n{attrs_str}\n>"
+        if isinstance(data, GeneratedModelWrapper):
+            self._model = data
+            self._data = self._model.items
 
-class ListCollection(Model):
-    _naming: str = "Collection"
-    _single: Callable = Single
-    _data: List[BaseModel] = None
-    
     @property
-    def _list(self) -> List[BaseModel]:
-        """Returns the reference list of items."""
+    def factory(self) -> Callable:
+        return GeneratedModelWrapper
+
+    @property
+    def data(self) -> List[BaseModel]:
+        """Returns the reference list of items inside model."""
         return self._data
 
-    def __init__(self, collection: List[BaseModel]):
-        """
-        Initializes the ItemCollection class.
-        
-        Args:
-            collection (List[BaseModel]): The collection of items.
-        """
-        self._data = collection
-        self._iterator = None
-        self._current = None
-    
-    @property
-    def items(self) -> List[Single]:
-        """
-        Returns the list of items in the collection.
-        
-        Returns:
-            List[Single]: A list of User objects.
-        """
-        if self._list is None:
-            return []
-        
-        for item in self._list:
-            yield self._single(item)
+    def __getitem__(self, idx) -> Any:
+        return self.factory(self.data[idx])
 
-    def __iter__(self):
-        """
-        Returns an iterator for the collection.
-
-        Usage:
-            collection = ItemCollection(...)
-            for item in collection:
-                print(collection.current)  # Always shows the current item
-        """
-        self._iterator = iter(self._list)
-        self._current = None            
-        return self
-
-    def __next__(self):
-        """
-        Returns the next item in the iteration.
-        """
-        self._current = next(self._iterator)
-        return self._current
-    
     def __len__(self):
-        """
-        Returns the number of items in the collection.
-        """
-        return len(self._list)
+        return len(self.data)
 
     @property
-    def current(self):
-        """
-        Returns the current item in the iteration.
-        """
-        return self._current
+    def first(self) -> GeneratedModelWrapper:
+        return self.factory(self.data[0])
 
-    @property
-    def all(self) -> List[Single]:
-        """
-        Returns all items in the collection as a list.
-
-        Returns:
-            List[Single]: A list of all Single objects.
-        """
-        return list(self.items)
-
-    @property
-    def total(self) -> int:
-        """
-        Returns the total number of records in the collection.
-
-        Returns:
-            int: The total number of records.
-        """
-        return self.__len__()
-    
-    def first(self, attr: str, value: Any) -> Single | None:
-        """
-        Returns the first item that matches the specified attribute and value.
-        
-        Args:
-            attr (str): The attribute to search by.
-            value (Any): The value to match.
-            
-        Returns:
-            Single | None: The first matching item, or None if not found.
-        """
-        for item in self.filter({attr: value}, limit=1).items:
-            return item
-        return None
-
-    def filter(self, criteria: dict, limit: int = None) -> 'ListCollection':
-        """
-        Returns a list of items that match the specified criteria.
-        
-        Args:
-            criteria (dict): A dictionary of attribute-value pairs to filter by.
-            limit (int, optional): The maximum number of items to return. Defaults to None (no limit).
-            
-        Returns:
-            List[Single]: A list of Single objects that match the criteria.
-        """
-        if self._list is None:
-            return []
-
-        result = []
-        for item in self._list:
-            match = True
-            for key, value in criteria.items():
-                if getattr(item, key, None) != value:
-                    match = False
-                    break
-            if match:
-                result.append(item)
-                if limit is not None and len(result) >= limit:
-                    break
-
-        return self.__class__(copy.deepcopy(result))
+    def filter(self, criteria: dict) -> Self:
+        return [
+            obj for obj in self.data
+            if all(getattr(obj, k, None) == v for k, v in criteria.items())
+        ]
 
     def __repr__(self) -> str:
         if len(self) == 0:
-            return f"<{self._naming} (no items)>"
+            return f"<{self.__class__.__name__} (no items)>"
 
-        items_repr = ""
-        size = 10
-        
-        for item in self.items:
-            size -= 1
-            if size < 0:
-                break
-
-            attrs = repr(item).split(',\n')
-            items_repr += ",\n".join(attrs[:10])
-
+        parts = []
+        for item in islice(self.items, 10):
+            attrs = repr(item).split(",\n")
+            parts.append(",\n".join(attrs[:10]))
             if len(attrs) > 10:
-                items_repr += ", ...\n"
+                parts.append(", ...\n")
 
         if len(self) > 10:
-            items_repr += "  ..."
-        return (
-            f"<{self._naming} total={self.total} items=[\n  {items_repr}\n]>"
-        )
+            parts.append("  ...")
+
+        return f"<{self.__class__.__name__} items=[\n  {',\n'.join(parts)}\n]>"
