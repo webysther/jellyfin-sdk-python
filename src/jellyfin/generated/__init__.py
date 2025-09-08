3ba033c0a0e842bda780a41c4c3a7515b34ccca4
Webysther Sperandio <webysther@gmail.com>
2025-09-08 18:01:45 +0200

    Fix a but to work with python 3.13 using the new REPL

--- src/jellyfin/generated/__init__.py
+++ src/jellyfin/generated/__init__.py
@@ -7,40 +7,39 @@ class Version(Enum):
     V10_11 = "10.11"
 
 class Proxy:
-    _VERSION = Version.V10_10
-
-    @property
-    def current(self) -> Version:
-        return self._VERSION
-
-    @classmethod
-    @current.setter
-    def current(cls, version: Version):
-        """Set the default API version for dynamic imports."""
-        cls._VERSION = version
+    current = Version.V10_10
+    modules = {}
 
     @classmethod
     @property
     def default(cls):
         """Dynamically imports the appropriate API module based on the specified version."""
-        return cls.module(cls._VERSION)
+        return cls.module(cls.current)
+
+    @classmethod
+    def name(cls, version: Version) -> str:
+        """Returns the API module name for the specified version."""
+        module_target = version.value.replace('.', '_')
+        return f"jellyfin.generated.api_{module_target}"
 
     @classmethod
     def module(cls, version: Version):
         """Dynamically imports and returns the appropriate API module based on the specified version."""
-        module_target = version.value.replace('.', '_')
-        module_name = f"jellyfin.generated.api_{module_target}"
-        return importlib.import_module(module_name)
+        return importlib.import_module(cls.name(version))
     
     @classmethod
     def factory(cls, name, version: Version):
         """Factory method to get a class or function from the specified version module."""
-        return getattr(cls.module(version), name)
+        if cls.name(version) not in cls.modules:
+            print(cls.name(version))
+            cls.modules[cls.name(version)] = cls.module(version)
+
+        return getattr(cls.modules[cls.name(version)], name)
 
 def __getattr__(name):
     """Dynamically fetch attributes from the default API version module."""
     try:
-        return getattr(Proxy.default, name)
+        return Proxy.factory(name, Proxy.current)
     except AttributeError:
         raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
 
